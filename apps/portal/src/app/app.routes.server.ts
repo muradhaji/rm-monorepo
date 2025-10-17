3af351449bc958ee8d3a97a455052a1a3cc37c23
muradhaji <muradhajiyev42@icloud.com>
2025-10-17 19:48:07 +0400

    RenderMode changed for 'characters/:id' route

--- apps/portal/src/app/app.routes.server.ts
+++ apps/portal/src/app/app.routes.server.ts
@@ -5,4 +5,8 @@ export const serverRoutes: ServerRoute[] = [
     path: '**',
     renderMode: RenderMode.Prerender,
   },
+  {
+    path: 'characters/:id',
+    renderMode: RenderMode.Server,
+  },
 ];
